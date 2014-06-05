53b28aef158d7e20623b16b64f2e3cc7e210c7f6
Severin Mueller <severin.mueller@gmx.net>
2014-06-05 20:54:40 +0200

    fixed error in script

--- inc/homeImpl.php
+++ inc/homeImpl.php
@@ -4,20 +4,19 @@ require_once(ROOT_PATH."/inc/IPage.php");
 
 class Home implements IPage {
 
-    private $pageTitle = "<span class=\"bigfont\">Ohne Sicherheit ist keine Freiheit<\/span>";
+	private $pageTitle = "<span class=\"bigfont\">Ohne Sicherheit ist keine Freiheit</span>";
 
-    public function getPageContent() {
-//        $text = "<span class=smallfont>Wilhelm von Humboldt (1767 - 1835), dt. Philosoph u. Sprachforscher, preuss. Staatsmann</span>";
-	$text = "bla";
-        return $text;
-    }
+	public function getPageContent() {
+		return '<span class=\"smallfont\">Wilhelm von Humboldt (1767 - 1835), dt. Philosoph u. Sprachforscher, preuss. Staatsmann</span>';
+	}
 
-    public function getPageTitle() {
-        return $this->pageTitle;
-    }
+	public function getPageTitle() {
+        	return $this->pageTitle;
+	}
 
-    public function setPageTitle($title) {
-        $this->pageTitle = $title;
+	public function setPageTitle($title) {
+		$this->pageTitle = $title;
+	}
 }
 
 ?>
